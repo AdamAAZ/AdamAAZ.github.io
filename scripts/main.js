b9f4d27c5c540178a97e8fe3fc0bf8d88af3e4d8
Adam.AZ <134951033+AdamAAZ@users.noreply.github.com>
2024-06-17 16:36:45 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+const falvourImage = document.querySelector("img");
+
+falvourImage.onclick = function () {
+    const flavourSrc = falvourImage.getAttribute("src");
+    if (flavourSrc === "images/monster-logo.png") {
+        falvourImage.setAttribute("src", "images/monster-flavours.jpg");
+    } 
+    else {
+        falvourImage.setAttribute("src", "images/monster-logo.png")
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h2");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    }
+    else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Release the beast, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Release the beast, ${storedName}`
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
+  
